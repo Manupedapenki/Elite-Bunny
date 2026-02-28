7209c8d4b9b121ca35e6491492aa791643aa6a7e
Manohar Pedapenki <manupedapenki@gmail.com>
2026-02-28 12:07:44 +0530

    Testing the Bunny bot analysis on new repo

--- security_test.py
+++ security_test.py
@@ -1,7 +1,7 @@
 import os
-# BUG 1: Hardcoded secret
+# BUG 1: Hardcoded secret (The bot should catch this)
 ADMIN_TOKEN = "secret_998877665544" 
 
-# BUG 2: Command Injection
+# BUG 2: Command Injection (The bot should catch this)
 def run_user_cmd(cmd):
     os.system("echo " + cmd)
