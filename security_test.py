997f2d102ee581bd3a6add4c3ca900fd35513831
Manohar Pedapenki <manupedapenki@gmail.com>
2026-03-01 12:49:43 +0530

    Testing the security bot analysis

--- security_test.py
+++ security_test.py
@@ -1,7 +1,7 @@
 import os
-# BUG 1: Hardcoded secret (The bot should catch this)
-ADMIN_TOKEN = "secret_998877665544" 
+# BUG 1: Hardcoded Secret
+ADMIN_PASSWORD = "super-secret-12345" 
 
-# BUG 2: Command Injection (The bot should catch this)
-def run_user_cmd(cmd):
-    os.system("echo " + cmd)
+# BUG 2: Command Injection
+def run_cmd(user_input):
+    os.system("echo " + user_input)
